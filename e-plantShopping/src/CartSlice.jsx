b1ae827ddfa68ae25f2bbfdfd22ac8ea96e26d95
MyriamAA <myriamabouatmeh@gmail.com>
2024-07-24 17:20:56 +0300

    Remove directory path/to/directory

---
+++ e-plantShopping/src/CartSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const plant = action.payload;
+      const { name, image, cost } = plant; // Ensure image is destructured here
+      const existingPlant = state.items.find((p) => p.name === name);
+      if (existingPlant) {
+        existingPlant.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      const name = action.payload;
+      state.items = state.items.filter((item) => item.name !== name);
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const plant = state.items.find((plant) => plant.name === name);
+      if (plant) {
+        plant.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
